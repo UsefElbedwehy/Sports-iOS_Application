3d5977af2d073193af2a1faf7c58e4241ee3b914
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-31 00:28:56 +0200

    add new functions to fetchData from teams, Upcoming matches and the latest matches

--- sportsApp/Model/service/Service.swift
+++ sportsApp/Model/service/Service.swift
@@ -8,8 +8,8 @@
 import Foundation
 protocol ApiProtocol {
     static func fetchLeaguesFromModel(leagueIndex:Int,compilationHandler: @escaping (Leagues?)->Void)
-    static func fetchFixturesFromModel(leagueIndex:Int,compilationHandler: @escaping (Fixtures?)->Void)
-    static func fetchTeamsFromModel(leagueIndex:Int,compilationHandler: @escaping (Teams?)->Void)
+    static func fetchFixturesFromModel(leagueIndex:Int,exten:[String],compilationHandler: @escaping (Fixtures?)->Void)
+    static func fetchTeamsFromModel(leagueIndex:Int,exten:String,compilationHandler: @escaping (Teams?)->Void)
 }
 class Service : ApiProtocol {
     static func fetchLeaguesFromModel(leagueIndex:Int,compilationHandler: @escaping(Leagues?) -> Void) {
@@ -38,8 +38,10 @@ class Service : ApiProtocol {
         }
         task.resume()
     }
-    static func fetchFixturesFromModel(leagueIndex:Int,compilationHandler: @escaping(Fixtures?) -> Void) {
-        let url = URL(string: "https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=1bdf9ca0bb2950913f68e460c14c228d8775b1266523146f4c14f76ea04b5678&leagueid=152&from=2025-01-25&to=2025-01-25")
+    static func fetchFixturesFromModel(leagueIndex:Int ,exten:[String],compilationHandler: @escaping(Fixtures?) -> Void) {
+//        let url = URL(string: "https://apiv2.allsportsapi.com/\(leagueIndex)/?met=Fixtures&APIkey=1bdf9ca0bb2950913f68e460c14c228d8775b1266523146f4c14f76ea04b5678&from=2025-01-25&to=2025-02-25&leagueId=\(exten)")
+        let url = URL(string: ApiKeys.createApiUrlWithExten(league: Sports.sports[leagueIndex] , parameter: ApiParameters().Fixtures, exten: exten))
+        print(url ?? "url")
         guard let url = url else{ return }
         let request = URLRequest(url: url)
         let session = URLSession(configuration: .default)
@@ -68,8 +70,10 @@ class Service : ApiProtocol {
         }
         task.resume()
     }
-    static func fetchTeamsFromModel(leagueIndex:Int,compilationHandler: @escaping(Teams?) -> Void) {
-        let url = URL(string: "https://apiv2.allsportsapi.com/football/?&met=Teams&teamId=96&APIkey=1bdf9ca0bb2950913f68e460c14c228d8775b1266523146f4c14f76ea04b5678")
+    static func fetchTeamsFromModel(leagueIndex:Int,exten:String,compilationHandler: @escaping(Teams?) -> Void) {
+//        let url = URL(string: "https://apiv2.allsportsapi.com/football/?&met=Teams&APIkey=1bdf9ca0bb2950913f68e460c14c228d8775b1266523146f4c14f76ea04b5678&leagueId=152")
+        let url = URL(string: ApiKeys.createApiUrlWithExten(league: Sports.sports[leagueIndex] , parameter: ApiParameters().Teams, exten: ["leagueId=\(exten)"]))
+        print(url ?? "url")
         guard let url = url else{ return }
         let request = URLRequest(url: url)
         let session = URLSession(configuration: .default)
