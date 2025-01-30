306fdec4185c631c55511b86fc25fb9c2b89b766
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-31 00:21:42 +0200

    add uicollectionViewCell for the upComing matches section

---
+++ sportsApp/View/Leagues/TopMatchCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  TopMatchCollectionViewCell.swift
+//  sportsApp
+//
+//  Created by Usef on 30/01/2025.
+//
+
+import UIKit
+
+class TopMatchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var stadiumNameLB: UILabel!
+    
+    @IBOutlet weak var homeImgView: UIImageView!
+    
+    @IBOutlet weak var awayImgView: UIImageView!
+    
+    @IBOutlet weak var awayTeamNameLB: UILabel!
+    
+    @IBOutlet weak var homeTeamNameLB: UILabel!
+    
+    @IBOutlet weak var matchDateLB: UILabel!
+    
+    @IBOutlet weak var matchTimeLB: UILabel!
+    
+    @IBOutlet weak var upcomingMatchesView: UIView!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
