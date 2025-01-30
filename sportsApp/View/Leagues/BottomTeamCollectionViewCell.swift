aad98e258fbc823d152432f91b8df300fa3acd30
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-31 00:21:02 +0200

    add uicollectionViewCell for the teams section

---
+++ sportsApp/View/Leagues/BottomTeamCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BottomTeamCollectionViewCell.swift
+//  sportsApp
+//
+//  Created by Usef on 30/01/2025.
+//
+
+import UIKit
+
+class BottomTeamCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var teamImgView: UIImageView!
+    
+    @IBOutlet weak var teamsCardView: UIView!
+}
