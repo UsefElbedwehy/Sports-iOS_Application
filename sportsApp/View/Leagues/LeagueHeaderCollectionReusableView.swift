f019a1fd345395252a5e62af65ff12d882cb7eac
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-31 13:46:36 +0200

    change the players placeholder

---
+++ sportsApp/View/Leagues/LeagueHeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  LeagueHeaderCollectionReusableView.swift
+//  sportsApp
+//
+//  Created by Usef on 31/01/2025.
+//
+
+import UIKit
+
+class LeagueHeaderCollectionReusableView: UICollectionReusableView {
+        
+}
