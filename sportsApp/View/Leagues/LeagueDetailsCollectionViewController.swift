1bb13a35911af4a5013a11264e9aede60170be75
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-01-31 13:50:13 +0200

    improve the modularity and readability of the code of the cards

--- sportsApp/View/Leagues/LeagueDetailsCollectionViewController.swift
+++ sportsApp/View/Leagues/LeagueDetailsCollectionViewController.swift
@@ -18,6 +18,24 @@ class LeagueDetailsCollectionViewController: UICollectionViewController ,LeagueP
     var fixturesArray = [Fixture]()
     var upComingArray = [Fixture]()
     var teamsArray = [Team]()
+    let sectionsHeader = ["Upcoming  matches","Latest matches","Teams"]
+    let awayPlaceHolder = "https://marketplace.canva.com/EAF7iHWkPBk/1/0/1600w/canva-blue-and-yellow-circle-modern-football-club-badge-logo-B4ALVxfLQS0.jpg"
+    let homePlaceHolder = "https://images-platform.99static.com/4UjFKF0lqaqTGtHZIUNsYeNeUak=/500x500/top/smart/99designs-contests-attachments/45/45950/attachment_45950568"
+    let teamLogoPlaceHolder = "https://e7.pngegg.com/pngimages/710/859/png-clipart-logo-football-team-football-logo-design-free-logo-design-template-label.png"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.clearsSelectionOnViewWillAppear = true
+        let presenter = Presenter()
+        presenter.attachToLeagueView(view: self)
+        presenter.FetchTeamsFromJson(leagueIndex,leagueID: leagueId)
+        presenter.FetchFixtureFromJson(leagueIndex,leagueID: leagueId)
+        presenter.FetchUpComingFixtureFromJson(leagueIndex,leagueID: leagueId)
+        UIHelper.addGradientSubViewToView(view: view)
+        compsitionalLayoutInit()
+        NavBarSetUp.setBackBtn(navigationItem: navigationItem, navController: navigationController!)
+        sleep(UInt32(0.18))
+    }
+    
     func renderLatestMatchesToView(res: Fixtures) {
         guard let result = res.result else {
                 print("Error: Fixtures result is nil")
@@ -50,20 +68,6 @@ class LeagueDetailsCollectionViewController: UICollectionViewController ,LeagueP
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.clearsSelectionOnViewWillAppear = true
-        let presenter = Presenter()
-        presenter.attachToLeagueView(view: self)
-        presenter.FetchTeamsFromJson(leagueIndex,leagueID: leagueId)
-        presenter.FetchFixtureFromJson(leagueIndex,leagueID: leagueId)
-        presenter.FetchUpComingFixtureFromJson(leagueIndex,leagueID: leagueId)
-        UIHelper.addGradientSubViewToView(view: view)
-        compsitionalLayoutInit()
-        NavBarSetUp.setBackBtn(navigationItem: navigationItem, navController: navigationController!)
-        sleep(UInt32(0.18))
-    }
-    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 3
     }
@@ -86,46 +90,24 @@ class LeagueDetailsCollectionViewController: UICollectionViewController ,LeagueP
         switch indexPath.section {
         case 0:
             let topMatchCardCell = collectionView.dequeueReusableCell(withReuseIdentifier: "topMatchCell", for: indexPath) as! TopMatchCollectionViewCell
-            UIHelper.removeExistingGradients(from: topMatchCardCell.upcomingMatchesView)
-            UIHelper.addGradientSubViewToCell(view: topMatchCardCell.upcomingMatchesView, at:0)
-            UIHelper.addGradientSubViewToMatchCardCell(view: topMatchCardCell.upcomingMatchesView, at: 0)
+            addGradientToUpComingMatchesCard(topMatchCardCell: topMatchCardCell)
             topMatchCardCell.homeTeamNameLB.text = upComingArray[indexPath.row].event_home_team
             topMatchCardCell.awayTeamNameLB.text = upComingArray[indexPath.row].event_away_team
             topMatchCardCell.matchDateLB.text = upComingArray[indexPath.row].event_date
             topMatchCardCell.matchTimeLB.text = upComingArray[indexPath.row].event_time
             topMatchCardCell.stadiumNameLB.text = upComingArray[indexPath.row].event_stadium
-            let homeUrl = URL(string: upComingArray[indexPath.row].home_team_logo ?? "https://images-platform.99static.com/4UjFKF0lqaqTGtHZIUNsYeNeUak=/500x500/top/smart/99designs-contests-attachments/45/45950/attachment_45950568")
-            let awayUrl = URL(string: upComingArray[indexPath.row].away_team_logo ?? "https://marketplace.canva.com/EAF7iHWkPBk/1/0/1600w/canva-blue-and-yellow-circle-modern-football-club-badge-logo-B4ALVxfLQS0.jpg")
-            topMatchCardCell.homeImgView.kf.setImage(with: homeUrl)
-            topMatchCardCell.awayImgView.kf.setImage(with: awayUrl)
+            setUpcomingTeamsImage(topMatchCardCell: topMatchCardCell, indexPath: indexPath)
             return topMatchCardCell
         case 1:
             let matchCardCell = collectionView.dequeueReusableCell(withReuseIdentifier: "lagueDCell", for: indexPath) as! MiddleMatchCollectionViewCell
-            UIHelper.removeExistingGradients(from: matchCardCell.matchCardView)
-            UIHelper.addGradientSubViewToCell(view: matchCardCell.matchCardView, at:0)
-            UIHelper.addGradientSubViewToMatchCardCell(view: matchCardCell.matchCardView, at: 0)
-            matchCardCell.matchCardView.backgroundColor = UIColor.black
-            matchCardCell.layer.cornerRadius = 15
+            addGradientToMatchCard(matchCardCell: matchCardCell)
             matchCardCell.staduimLabel.text = fixturesArray[indexPath.row].event_stadium
-            matchCardCell.homeTeamLB.text = fixturesArray[indexPath.row].event_home_team
-            matchCardCell.awayTeamLB.text = fixturesArray[indexPath.row].event_away_team
-            matchCardCell.matchDateLB.text = fixturesArray[indexPath.row].event_date
-            matchCardCell.matchTimeLB.text = fixturesArray[indexPath.row].event_time
-            var matchScore:[String] = (fixturesArray[indexPath.row].event_ft_result?.components(separatedBy: "-")) ?? ["0", "0"]
-            if matchScore.count == 0 {
-                matchScore.append("0")
-                matchScore.append("0")
-            }else if matchScore.count == 1{
-                matchScore[0] = "0"
-                matchScore.append("0")
-            }
-                matchCardCell.homeTeamScoreLB.text = matchScore[0]
-                matchCardCell.awayTeamScoreLB.text = matchScore[1]
-            
-            let homeUrl = URL(string: fixturesArray[indexPath.row].home_team_logo ?? "https://images-platform.99static.com/4UjFKF0lqaqTGtHZIUNsYeNeUak=/500x500/top/smart/99designs-contests-attachments/45/45950/attachment_45950568")
-            let awayUrl = URL(string: fixturesArray[indexPath.row].away_team_logo ?? "https://marketplace.canva.com/EAF7iHWkPBk/1/0/1600w/canva-blue-and-yellow-circle-modern-football-club-badge-logo-B4ALVxfLQS0.jpg")
-            matchCardCell.homeTeamImgView.kf.setImage(with: homeUrl)
-            matchCardCell.awayTeamImgView.kf.setImage(with: awayUrl)
+            matchCardCell.homeTeamLB.text   = fixturesArray[indexPath.row].event_home_team
+            matchCardCell.awayTeamLB.text   = fixturesArray[indexPath.row].event_away_team
+            matchCardCell.matchDateLB.text  = fixturesArray[indexPath.row].event_date
+            matchCardCell.matchTimeLB.text  = fixturesArray[indexPath.row].event_time
+            setMatchScore(matchCardCell: matchCardCell, indexPath: indexPath)
+            setLatestTeamsImage(matchCardCell: matchCardCell, indexPath: indexPath)
             return matchCardCell
         default:
             let teamCardCell = collectionView.dequeueReusableCell(withReuseIdentifier: "bottomTeamCell", for: indexPath) as! BottomTeamCollectionViewCell
@@ -134,12 +116,69 @@ class LeagueDetailsCollectionViewController: UICollectionViewController ,LeagueP
             UIHelper.addGradientSubViewToTeamsCell(view: teamCardCell.teamsCardView, at: 0)
             teamCardCell.backgroundColor = UIColor.black
             teamCardCell.layer.cornerRadius = 15
-            let teamUrl = URL(string: teamsArray[indexPath.row].team_logo ?? "https://e7.pngegg.com/pngimages/710/859/png-clipart-logo-football-team-football-logo-design-free-logo-design-template-label.png")
-            teamCardCell.teamImgView.kf.setImage(with: teamUrl)
+            setTeamLogoImage(teamCardCell: teamCardCell, indexPath: indexPath)
             return teamCardCell
         }
     }
-
+    func addGradientToMatchCard(matchCardCell:MiddleMatchCollectionViewCell){
+        UIHelper.removeExistingGradients(from: matchCardCell.matchCardView)
+        UIHelper.addGradientSubViewToCell(view: matchCardCell.matchCardView, at:0)
+        UIHelper.addGradientSubViewToMatchCardCell(view: matchCardCell.matchCardView, at: 0)
+        matchCardCell.matchCardView.backgroundColor = UIColor.black
+        matchCardCell.layer.cornerRadius = 15
+    }
+    func addGradientToUpComingMatchesCard(topMatchCardCell:TopMatchCollectionViewCell){
+        UIHelper.removeExistingGradients(from: topMatchCardCell.upcomingMatchesView)
+        UIHelper.addGradientSubViewToCell(view: topMatchCardCell.upcomingMatchesView, at:0)
+        UIHelper.addGradientSubViewToMatchCardCell(view: topMatchCardCell.upcomingMatchesView, at: 0)
+        topMatchCardCell.upcomingMatchesView.backgroundColor = UIColor.black
+        topMatchCardCell.layer.cornerRadius = 15
+    }
+    func setMatchScore(matchCardCell:MiddleMatchCollectionViewCell , indexPath:IndexPath){
+        var matchScore:[String] = (fixturesArray[indexPath.row].event_final_result?.components(separatedBy: "-")) ?? ["0", "0"]
+        if matchScore.count == 0 {
+            matchScore.append("0")
+            matchScore.append("0")
+        }else if matchScore.count == 1{
+            matchScore[0] = "0"
+            matchScore.append("0")
+        }
+            matchCardCell.homeTeamScoreLB.text = matchScore[0]
+            matchCardCell.awayTeamScoreLB.text = matchScore[1]
+    }
+    func setLatestTeamsImage(matchCardCell:MiddleMatchCollectionViewCell , indexPath:IndexPath){
+        if let homeUrl = URL(string: fixturesArray[indexPath.row].home_team_logo ?? homePlaceHolder){
+            matchCardCell.homeTeamImgView.kf.setImage(with: homeUrl)
+        }else{
+            matchCardCell.homeTeamImgView.image = UIImage(named: "HomeTeamLogoPlaceHolder")
+        }
+        if let awayUrl = URL(string: fixturesArray[indexPath.row].away_team_logo ?? awayPlaceHolder){
+            matchCardCell.awayTeamImgView.kf.setImage(with: awayUrl)
+        }else{
+            matchCardCell.awayTeamImgView.image = UIImage(named: "AwayTeamLogoPlaceHolder")
+        }
+    }
+    func setUpcomingTeamsImage(topMatchCardCell:TopMatchCollectionViewCell , indexPath:IndexPath){
+        if let homeUrl = URL(string: upComingArray[indexPath.row].home_team_logo ?? homePlaceHolder){
+            topMatchCardCell.homeImgView.kf.setImage(with: homeUrl)
+        }else{
+            topMatchCardCell.homeImgView.image = UIImage(named: "HomeTeamLogoPlaceHolder")
+        }
+        if let awayUrl = URL(string: upComingArray[indexPath.row].away_team_logo ?? awayPlaceHolder){
+            topMatchCardCell.awayImgView.kf.setImage(with: awayUrl)
+        }else{
+            topMatchCardCell.awayImgView.image = UIImage(named: "AwayTeamLogoPlaceHolder")
+        }
+    }
+    func setTeamLogoImage(teamCardCell:BottomTeamCollectionViewCell , indexPath:IndexPath){
+        if let teamUrl = URL(string: teamsArray[indexPath.row].team_logo ?? teamLogoPlaceHolder){
+            teamCardCell.teamImgView.kf.setImage(with: teamUrl)
+        }else{
+            teamCardCell.teamImgView.image = UIImage(named: "HomeTeamLogoPlaceHolder")
+        }
+    }
+    
+    
     func compsitionalLayoutInit() {
         let layout = UICollectionViewCompositionalLayout { sectionIndex , enviroment in
             switch sectionIndex {
@@ -178,19 +217,30 @@ class LeagueDetailsCollectionViewController: UICollectionViewController ,LeagueP
         group.contentInsets = NSDirectionalEdgeInsets(top: 10.0, leading: 13.0, bottom: 10.0, trailing: 13.0)
         let section = NSCollectionLayoutSection(group: group)
         section.contentInsets = NSDirectionalEdgeInsets(top: 10.0, leading: 10.0, bottom: 10.0, trailing: 10.0)
+        let headerSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(0.95),
+                heightDimension: .absolute(30)
+            )
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        section.boundarySupplementaryItems = [header]
         return section
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if indexPath.section == 2 {
-            //nav to players
             let teamVC = self.storyboard?.instantiateViewController(identifier: "teamVC") as! TeamDetailsViewController
             teamVC.teamID = teamsArray[indexPath.row].team_key ?? 50
             teamVC.playersArray = teamsArray[indexPath.row].players!
+            teamVC.navigationItem.title = teamsArray[indexPath.row].team_name ?? "team"
             self.navigationController?.pushViewController(teamVC, animated: true)
             
         }
     }
+    
     
     // MARK: UICollectionViewDelegate
 
@@ -223,4 +273,9 @@ class LeagueDetailsCollectionViewController: UICollectionViewController ,LeagueP
     }
     */
 
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "leagueSectionsHeader", for: indexPath) as! LeagueHeaderCollectionReusableView
+        header.headerLB.text = sectionsHeader[indexPath.section]
+        return header
+    }
 }
