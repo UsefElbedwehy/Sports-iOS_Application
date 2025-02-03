b17a37b735867e90d900a28e0540f6066b555ae1
Usef Elbedwehy <elbedwehyy@gmail.com>
2025-02-03 10:43:43 +0200

    adding a custom cell for the team card in the players details screen

---
+++ sportsApp/View/Leagues/TeamDetials/TeamCellInTeamDetails.swift
@@ -0,0 +1,23 @@
+//
+//  TeamCellInTeamDetails.swift
+//  sportsApp
+//
+//  Created by Usef on 02/02/2025.
+//
+
+import UIKit
+
+class TeamCellInTeamDetails: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
